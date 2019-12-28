eda7ef7670e7a6009359d099f1b142ab8e3cfcaa
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-12-28 21:51:42 +0100

    fix: use only two ICE servers

--- src/module.ts
+++ src/module.ts
@@ -12,10 +12,7 @@ export * from './types/index';
 const ICE_SERVERS = [ {
     urls: [
         'stun:stun.l.google.com:19302',
-        'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun3.l.google.com:19302',
-        'stun:stun4.l.google.com:19302'
+        'stun:global.stun.twilio.com:3478?transport=udp'
     ]
 } ];
 
